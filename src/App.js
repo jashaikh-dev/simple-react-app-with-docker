78fd4fd7368042448b09d7fcee1a91908d78a50d
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-03-02 21:45:12 -0500

    feat: react & docker init

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React in Docker
+          <br />
+          This is Awesome!
         </p>
         <a
           className="App-link"
